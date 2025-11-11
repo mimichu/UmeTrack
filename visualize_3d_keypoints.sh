a54ad4c8c6f876ba4c31c72fd7e2ec0ae0181534
mimichu <chuer.pan@gmail.com>
2025-11-10 23:36:25 -0800

    update command for fs for umetrack data

--- visualize_3d_keypoints.sh
+++ visualize_3d_keypoints.sh
@@ -4,7 +4,7 @@
 #     --json ~/Documents/ZED/zed_umetrack_full_format.json \
 #     --model pretrained_models/pretrained_weights.torch
 
-# python3 run_inference_zed.py \
+# python3 /home/chuerpan/repo/InteractionRetarget/submodules/UmeTrack/run_inference_zed.py \
 #     --input-file /home/chuerpan/repo/InteractionRetarget/submodules/UmeTrack/UmeTrack_data/raw_data/real/hand_hand/testing/user_05/recording_00.mp4 \
 #     --model-path pretrained_models/pretrained_weights.torch 
 
@@ -24,8 +24,19 @@
 #     --video UmeTrack_data/raw_data/real/hand_hand/training/user_00/recording_00.mp4 \
 #     --predictions /home/chuerpan/repo/InteractionRetarget/submodules/UmeTrack/tmp/eval_results_known_skeleton/real/hand_hand/testing/user_05/recording_00.npy
 
+# python visualize_3d_keypoints.py \
+#     --video UmeTrack_data/raw_data/real/hand_hand/training/user_00/recording_00.mp4 \
+#     --model pretrained_models/pretrained_weights.torch \
+#     --generic-hand-model dataset/generic_hand_model.json
 
-python visualize_3d_keypoints.py \
-    --video UmeTrack_data/raw_data/real/hand_hand/training/user_00/recording_00.mp4 \
-    --model pretrained_models/pretrained_weights.torch \
-    --generic-hand-model dataset/generic_hand_model.json
+svo_name="real_separate_hand_testing_user_19_recording_01_cam1_images"
+frame_idx="000000"
+
+python /home/chuerpan/repo/InteractionRetarget/submodules/FoundationStereo/scripts/run_demo.py \
+    --left_file /home/chuerpan/repo/InteractionRetarget/submodules/UmeTrack/UmeTrack_data/raw_data/real/separate_hand/testing/user_19/recording_01_cam1_images/recording_01_cam1_images_${frame_idx}_rectified.png \
+    --right_file /home/chuerpan/repo/InteractionRetarget/submodules/UmeTrack/UmeTrack_data/raw_data/real/separate_hand/testing/user_19/recording_01_cam1_images/recording_01_cam2_images_${frame_idx}_rectified.png \
+    --intrinsic_file /home/chuerpan/repo/InteractionRetarget/cam_configs/umetrack/recording_01_intrinsics.txt \
+    --ckpt_dir /home/chuerpan/repo/InteractionRetarget/submodules/FoundationStereo/pretrained_models/23-51-11/model_best_bp2.pth \
+    --out_dir /home/chuerpan/repo/InteractionRetarget/submodules/FoundationStereo/test_outputs/UmeTrack/${svo_name}_${frame_idx}/ \
+    --hiera 1 \
+    --remove_invisible 0 
